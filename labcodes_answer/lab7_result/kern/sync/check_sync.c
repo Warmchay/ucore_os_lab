d507614d2834960895d164623927cabfeadf41a1
1399852153 <139985215.@qq.com>
2020-12-15 22:36:58 +0800

    完善lab7注释

--- labcodes_answer/lab7_result/kern/sync/check_sync.c
+++ labcodes_answer/lab7_result/kern/sync/check_sync.c
@@ -85,32 +85,49 @@ struct proc_struct *philosopher_proc_sema[N];
 
 void phi_test_sema(i) /* i：哲学家号码从0到N-1 */
 { 
+	// 当哲学家i处于饥饿状态(HUNGRY),且其左右临近的哲学家都没有在就餐状态(EATING)
     if(state_sema[i]==HUNGRY&&state_sema[LEFT]!=EATING
             &&state_sema[RIGHT]!=EATING)
     {
+    	// 哲学家i饿了(HUNGRY)，且左右两边的叉子都没人用。
+    	// 令哲学家进入就餐状态（EATING）
         state_sema[i]=EATING;
+        // 唤醒阻塞在对应信号量上的哲学家线程
         up(&s[i]);
     }
 }
 
 void phi_take_forks_sema(int i) /* i：哲学家号码从0到N-1 */
 { 
-        down(&mutex); /* 进入临界区 */
-        state_sema[i]=HUNGRY; /* 记录下哲学家i饥饿的事实 */
-        phi_test_sema(i); /* 试图得到两只叉子 */
-        up(&mutex); /* 离开临界区 */
-        down(&s[i]); /* 如果得不到叉子就阻塞 */
+	// 拿叉子时需要通过mutex信号量进行互斥，防止并发问题(进入临界区)
+    down(&mutex);
+    // 记录下哲学家i饥饿的事实(执行phi_take_forks_sema尝试拿叉子，说明哲学家i进入了HUNGRY饥饿状态)
+    state_sema[i]=HUNGRY;
+    // 试图得到两只叉子
+    phi_test_sema(i);
+    // 离开临界区
+    up(&mutex);
+    // phi_test_sema中如果成功拿到叉子进入了就餐状态，会先执行up(&s[i])，再执行down(&s[i])时便不会阻塞
+    // 反之，如果phi_test_sema中没有拿到叉子，则down(&s[i])将会令哲学家i阻塞在信号量s[i]上
+    down(&s[i]);
 }
 
 void phi_put_forks_sema(int i) /* i：哲学家号码从0到N-1 */
 { 
-        down(&mutex); /* 进入临界区 */
-        state_sema[i]=THINKING; /* 哲学家进餐结束 */
-        phi_test_sema(LEFT); /* 看一下左邻居现在是否能进餐 */
-        phi_test_sema(RIGHT); /* 看一下右邻居现在是否能进餐 */
-        up(&mutex); /* 离开临界区 */
+	// 放叉子时需要通过mutex信号量进行互斥，防止并发问题(进入临界区)
+    down(&mutex); /* 进入临界区 */
+    // 哲学家进餐结束(执行phi_put_forks_sema放下叉子，说明哲学家已经就餐完毕，重新进入THINKING思考状态)
+    state_sema[i]=THINKING;
+    // 当哲学家i就餐结束，放下叉子时。需要判断左、右临近的哲学家在自己就餐的这段时间内是否也进入了饥饿状态，却因为自己就餐拿走了叉子而无法同时获得左右两个叉子。
+    // 为此哲学家i在放下叉子后需要尝试着判断在自己放下叉子后，左/右临近的、处于饥饿的哲学家能否进行就餐,如果可以就唤醒阻塞的哲学家线程，并令其进入就餐状态(EATING)
+    phi_test_sema(LEFT); /* 看一下左邻居现在是否能进餐 */
+    phi_test_sema(RIGHT); /* 看一下右邻居现在是否能进餐 */
+    up(&mutex); /* 离开临界区 */
 }
 
+/**
+ * 哲学家线程主体执行逻辑
+ * */
 int philosopher_using_semaphore(void * arg) /* i：哲学家号码，从0到N-1 */
 {
     int i, iter=0;
@@ -119,11 +136,16 @@ int philosopher_using_semaphore(void * arg) /* i：哲学家号码，从0到N-1 
     while(iter++<TIMES)
     { /* 无限循环 */
         cprintf("Iter %d, No.%d philosopher_sema is thinking\n",iter,i); /* 哲学家正在思考 */
+        // 使用休眠阻塞来模拟思考(哲学家线程阻塞N秒)
         do_sleep(SLEEP_TIME);
+        // 哲学家尝试着去拿左右两边的叉子(如果没拿到会阻塞)
         phi_take_forks_sema(i); 
         /* 需要两只叉子，或者阻塞 */
         cprintf("Iter %d, No.%d philosopher_sema is eating\n",iter,i); /* 进餐 */
+        // 使用休眠阻塞来模拟进餐(哲学家线程阻塞N秒)
         do_sleep(SLEEP_TIME);
+        // 哲学家就餐结束，将叉子放回桌子。
+        // 当发现之前有临近的哲学家尝试着拿左右叉子就餐时却没有成功拿到，尝试着唤醒对应的哲学家
         phi_put_forks_sema(i); 
         /* 把两把叉子同时放回桌子 */
     }
